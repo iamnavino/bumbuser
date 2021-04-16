75585f2d49bd98f6da00412675b89b9257cc4426
Hemant Patidar <iamhemant.indore@gmail.com>
2021-04-16 17:14:50 +0530

    Build 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'brodcast';
+
+  stream: boolean = false;
+  embededUrl: any;
+  constructor(public http: HttpClient,private sanitizer: DomSanitizer) {
+
+  }
+
+
+
+  startStream() {
+    this.stream = true;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/vnd.bambuser.v1+json',
+      'Authorization': 'Bearer GF1CJ6J6kfwbQmjk5mLA6V',
+    });
+
+    this.http.post('https://api.bambuser.com/broadcasterEmbedUrl',
+      '{"applicationId": "bI1ca3azlqV8LDUlIZHeug", "domain": "staging.sairyo.org"}'
+      , { headers: headers }).subscribe((data: any) => {
+
+        this.embededUrl = this.sanitizer.bypassSecurityTrustResourceUrl(data.embedUrl);
+      })
+  }
 }
